3c87b4dc7e72ce3edbcbb32aa403b2b1277c6528
Daniil Tamazlykar <daniiltamazlykar@gmail.com>
2018-05-21 09:25:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const ipRegex = require("ip-regex");
+var ipRegex = require("ip-regex");
 
-const v4 = ipRegex.v4().source + "\\/(3[0-2]|[12]?[0-9])";
-const v6 = ipRegex.v6().source + "\\/(12[0-8]|1[01][0-9]|[1-9]?[0-9])";
+var v4 = ipRegex.v4().source + "\\/(3[0-2]|[12]?[0-9])";
+var v6 = ipRegex.v6().source + "\\/(12[0-8]|1[01][0-9]|[1-9]?[0-9])";
 
-const ip = module.exports = opts => opts && opts.exact ?
+var ip = module.exports = opts => opts && opts.exact ?
   new RegExp(`(?:^${v4}$)|(?:^${v6}$)`) :
   new RegExp(`(?:${v4})|(?:${v6})`, "g");
 
